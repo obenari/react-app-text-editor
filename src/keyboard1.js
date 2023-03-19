1989843ffd42ed2334025c3c71ab6934d0249453
da077 <da077@DESKTOP-HV19KNK>
2023-03-19 13:12:09 +0200

    layout

--- src/keyboard1.js
+++ src/keyboard1.js
@@ -12,21 +12,40 @@ class Keybord1 extends Component {
       selectedKey: "",
       keyboardLayout: englishLayout,
     };
+    this.checKey = this.checKey.bind(this);
   }
-  handleKeyClick(key) {
-    this.setState({ selectedKey: key });
+
+  checKey(key) {
+    switch (key) {
+      case "CapsLock": {
+        if (this.state.keyboardLayout === englishLayout) {
+          this.setState({ keyboardLayout: englishLayoutUpperCase });
+        } else {
+          this.setState({ keyboardLayout: englishLayout });
+        }
+        break;
+      }
+      case "Tab": {
+        this.props.onPress("tab");
+        break;
+      }
+      case "Enter": {
+        this.props.onPress("\n");
+        break;
+      }
+      default: {
+        this.props.onPress(key);
+      }
+    }
   }
+
   render() {
     return (
       <div>
         <div className="keyboard">
           {this.state.keyboardLayout.map((row) =>
             row.map((key) => (
-              <button
-                key={key}
-                onClick={() => this.props.onPress(key)}
-                className="keyStyle"
-              >
+              <button key={key} onClick={() => this.checKey(key)}>
                 {key}
               </button>
             ))
